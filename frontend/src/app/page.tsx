9a058c069ab06b440aa5420d3c5e77afffbe2086
adibahbab4108 <adibalvi476@gmail.com>
2025-09-30 16:08:27 +0600

    chore: initialize frontend project with Next.js setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className=" relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        Hello my portfolio
+      </div>
+    </main>
+  );
+}
